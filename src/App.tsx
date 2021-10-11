b5310ebbb659a161d964987fce654d94d2ce3b87
Giorgio Turrini <gioturrini@hotmail.com>
2021-10-11 19:47:00 +0200

    SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ErrorBoundary } from 'react-error-boundary';
+import { RecoilRoot } from 'recoil';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <ErrorBoundary>
+
+      </ErrorBoundary>
+
+    </RecoilRoot>
   );
 }
 
